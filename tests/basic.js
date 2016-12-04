bc2c6e0db688dfefcdbb95a7194c89c01271e608
Chris Alderson <chrisalderson@protonmail.com>
2016-12-04 14:27:54 +0100

    semicolons

--- tests/basic.js
+++ tests/basic.js
@@ -25,22 +25,22 @@ function load() {
 tape('loads', t => {
   const P = load();
 
-  t.ok(P, 'we were able to load')
+  t.ok(P, 'we were able to load');
 
   const I = new P(config.args);
 
-  t.ok(I, 'we were able to instanciate')
+  t.ok(I, 'we were able to instanciate');
 
-  t.ok(I.config.name, 'we have a name')
-  t.ok(I.config.uniqueId, 'we have a uniqueId')
-  t.ok(I.config.tabName, 'we have a tabName')
-  t.ok(I.config.type, 'we have a type')
+  t.ok(I.config.name, 'we have a name');
+  t.ok(I.config.uniqueId, 'we have a uniqueId');
+  t.ok(I.config.tabName, 'we have a tabName');
+  t.ok(I.config.type, 'we have a type');
 
   t.end();
 });
 
 tape('fetch', t => {
-  debug('fetch, timeout', timeout)
+  debug('fetch, timeout', timeout);
   t.timeoutAfter(timeout);
 
   const P = load();
@@ -49,12 +49,12 @@ tape('fetch', t => {
   I.fetch({
     page: 1
   }).then(res => {
-    debug ('fetch', res)
-    t.ok(res, 'we were able to fetch')
-    t.ok(res.hasMore === true || res.hasMore === false, 'we have a hasMore field that is a boolean: ')
-    t.ok(res.results, 'we have a results field')
-    t.ok(res.results.length > 0, 'we have at least 1 result')
-    t.ok(I.extractIds(res), 'extractIds')
+    debug ('fetch', res);
+    t.ok(res, 'we were able to fetch');
+    t.ok(res.hasMore === true || res.hasMore === false, 'we have a hasMore field that is a boolean: ');
+    t.ok(res.results, 'we have a results field');
+    t.ok(res.results.length > 0, 'we have at least 1 result');
+    t.ok(I.extractIds(res), 'extractIds');
     t.end();
   }).catch(err => t.notOk(err, 'failed fetch'));
 });
